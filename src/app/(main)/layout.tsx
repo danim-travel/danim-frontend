5357b818a6da413f6545e1f5d062cf0dc62ad734
GAMMJ <gammjmj@gmail.com>
2026-06-17 11:56:15 +0900

    feat: SideNav 알림 뱃지를 WS 실시간 구독으로 연결 (#131)
    - NotificationBadgeSocket 컴포넌트로 useNotificationBadge 1회 마운트
    - (main)/layout.tsx AuthGuard 하위에 마운트 (인증 영역에서만 활성)
    - SideNav가 useNotificationsQuery 첫 페이지 파생 대신 notificationBadgeStore 구독

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -6,10 +6,12 @@ import SideNav from '@/components/SideNav'
 import { AuthGuard } from './_components/AuthGuard'
 import { SearchDrawer } from './_components/SearchDrawer'
 import { NotificationDrawer } from './_components/NotificationDrawer'
+import { NotificationBadgeSocket } from './_components/NotificationBadgeSocket'
 
 export default function MainLayout({ children }: { children: React.ReactNode }) {
   return (
     <AuthGuard>
+      <NotificationBadgeSocket />
       <div className="flex h-full relative">
         <div className="relative z-(--z-sidenav) shrink-0 h-full">
           <SideNav />
